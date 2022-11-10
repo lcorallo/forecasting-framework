2e307e6b02265ad5d76c3c300b26881f9cc46945
lcorallo <l.corallo2@gmail.com>
2022-11-10 19:20:10 +0100

    Adding new Regressor Interface, Refactoring Models

--- models/regressor/__init__.py
+++ models/regressor/__init__.py
@@ -1,4 +1,4 @@
+from .IRegressor import IRegressor
 from .linear_regression import Model_LinearRegression
 from .svr import Model_RBF_SVR
 from .svr import Model_Linear_SVR
-from .svr import Model_Polynomial_SVR
