bad362854b7745cd73639cc19d7318e23cd72fac
lcorallo <l.corallo2@gmail.com>
2022-11-12 15:06:31 +0100

    Improvement graph performance

--- test/pipeline/__init__.py
+++ test/pipeline/__init__.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 from src.models.regressor.linear_regression import Model_LinearRegression
 from src.performer.supervised_learning_performer import SlidingWindowPerformer
 from src.performer.train_test_performer import TrainTestPerformer
@@ -25,50 +27,81 @@ class Test_ARLinearRegression(IPipeline):
         ERROR_PERFORMER = ForecastErrorEvaluation(goal = goal)
         test_suite = TestSuite()
 
-        #Model Iper-parameters    
+        DIR = "test/output/ar_linear_regression/"
+        FILE = "data.csv"
+        OUTPUT = DIR + FILE
+
+        csv_output = pd.DataFrame(columns=[
+            "ID", "Series", "Target Length", "Target Offset", "AR Features", "Residuals Train", "Error Train", "Residuals Test", "Error Test"
+        ])
+
+        #Model Iper-parameters
         LINEAR_REGRESSION_IPER_PARAMETERS = ModelsIperParameters(
             FEATURE_LENGTH=[3,4,5,6,7,8,9,10,15,20]
         )
 
+        TARGET_LENGTH = [1]
+        TARGET_OFFSET = [1,2,3,4,5,11,12,13,14,15,21,22,23,24,25]
+
         number_of_experiment = test_suite.__get_test_suite_size__()
-        forecast_view, forecast_offset = goal.options()
         testIdAutoincrement = 0
 
-        for ind_experiment in number_of_experiment:
-            series = test_suite.__get_numpy_test_series_from_index__(ind_experiment)
-            series = MinMaxTransformer.transform(series)
-            series_name = test_suite.__get_name_test_series_from_index__(ind_experiment)
+        for ind_target_l in TARGET_LENGTH:
+            for ind_target_of in TARGET_OFFSET:
+                for ind_experiment in range(number_of_experiment):
+                    series = test_suite.__get_numpy_test_series_from_index__(ind_experiment)
+                    series = MinMaxTransformer.transform(series)
+                    series_name = test_suite.__get_name_test_series_from_index__(ind_experiment)
 
-            for ind_feature in LINEAR_REGRESSION_IPER_PARAMETERS.get(ModelsIperParameters.FEATURE_LENGTH):
-                SWP = SlidingWindowPerformer(
-                    feature_length=ind_feature,
-                    target_length = forecast_view,
-                    target_offset = forecast_offset
-                )
-                _, X, Y = SWP.get(series)
+                    for ind_feature in LINEAR_REGRESSION_IPER_PARAMETERS.get(ModelsIperParameters.FEATURE_LENGTH):
+                        SWP = SlidingWindowPerformer(
+                            feature_length = ind_feature,
+                            target_length = ind_target_l,
+                            target_offset = ind_target_of
+                        )
+                        _, X, Y = SWP.get(series)
 
-                TTP = TrainTestPerformer(portion_train = 0.8, random_sampling = True)
-                X_train, X_test, Y_train, Y_test = TTP.get(X, Y)
-                
-                model_linear_regression = Model_LinearRegression(input_size = X_train.shape[1])
-                yhat_train = model_linear_regression.__train__(X_train, Y_train)
-                yhat_test = model_linear_regression.__test__(X_test)
+                        TTP = TrainTestPerformer(portion_train = 0.8, random_sampling = True)
+                        X_train, X_test, Y_train, Y_test = TTP.get(X, Y)
+                        
+                        model_linear_regression = Model_LinearRegression(input_size = X_train.shape[1])
+                        yhat_train = model_linear_regression.__train__(X_train, Y_train)
+                        yhat_test = model_linear_regression.__test__(X_test)
 
-                error_train = ERROR_PERFORMER.get(Y_train, yhat_train)
-                error_test = ERROR_PERFORMER.get(Y_test, yhat_test)
-                
-                save_performance_graph(
-                    id = testIdAutoincrement,
-                    series = series,
-                    y_train = Y_train,
-                    y_test = Y_test,
-                    series_name = series_name,
-                    error_train = error_train,
-                    error_test = error_test,
-                    yhat_train = yhat_train,
-                    yhat_test = yhat_test
-                )
-                testIdAutoincrement += 1
+                        error_train = ERROR_PERFORMER.get(Y_train, yhat_train)
+                        error_test = ERROR_PERFORMER.get(Y_test, yhat_test)
+
+                        #Only for plotting:
+                        yhat_series = model_linear_regression.__test__(X)
+
+                        new_row = {
+                            'ID': testIdAutoincrement,
+                            'Series': series_name,
+                            "Target Length": ind_target_l,
+                            "Target Offset": ind_target_of,
+                            'AR Features': ind_feature,
+                            'Residuals Train': np.mean(np.abs(Y_train - yhat_train)),
+                            'Error Train': error_train,
+                            "Residuals Test": np.mean(np.abs(Y_test - yhat_test)),
+                            'Error Test': error_test
+                        }
+                        csv_output = csv_output.append(new_row, ignore_index=True)
+                        
+                        save_performance_graph(
+                            path = DIR,
+                            id = testIdAutoincrement,
+                            series = Y,
+                            yhat_series = yhat_series,
+                            y_train = Y_train,
+                            y_test = Y_test,
+                            series_name = series_name,
+                            error_train = error_train,
+                            error_test = error_test,
+                            yhat_train = yhat_train,
+                            yhat_test = yhat_test
+                        )
+                        testIdAutoincrement += 1
+        csv_output.to_csv(OUTPUT)
 
 
 
--- test/util/__init__.py
+++ test/util/__init__.py
@@ -4,50 +4,53 @@ from .test_suite import TestSuite
 
 
 def save_performance_graph(
-    path,
-    id,
-    series,
-    series_name,
-    y_train,
-    yhat_train,
-    y_test,
-    yhat_test,
-    rmse_train,
-    rmse_test):
-    # def showExperiment(ID, mixed, series, series_name, Y_train, Y_test, Yhat_train, Yhat_test, Yhat_series, rmse_train, rmse_test):
+        path,
+        id,
+        series,
+        yhat_series,
+        series_name,
+        y_train,
+        yhat_train,
+        y_test,
+        yhat_test,
+        error_train,
+        error_test
+    ):
+
+    formatted_error_train = f'{error_train:.5f}'
+    formatted_error_test = f'{error_test:.5f}'
+
     plt.figure(figsize=(30,10))
     plt.subplot(311)
-    plt.margins(0.05)
+    plt.margins(0.02)
     plt.plot(series)
-    # plt.plot(Yhat_series)
+    plt.plot(yhat_series, linestyle='--')
     plt.legend(["True", "Predicted"])
     plt.title(series_name)
 
     plt.subplot(323)
-    plt.margins(0.05)
+    plt.margins(0.02)
     plt.plot(y_train)
-    plt.plot(yhat_train)
+    plt.plot(yhat_train, linestyle='--', linewidth=1, marker='o', markersize=4)
     plt.legend(["True", "Predicted"])
-    plt.title("RMSE Train: {rmse_train:.5f}")
+    plt.title("RMSE Train: "+str(formatted_error_train))
 
     plt.subplot(324)
-    plt.margins(0.05)
+    plt.margins(0.02)
     plt.plot(y_test)
-    plt.plot(yhat_test)
+    plt.plot(yhat_test, linestyle='--', linewidth=1, marker='o', markersize=4)
     plt.legend(["True", "Predicted"])
-    plt.title("RMSE Test: {rmse_test:.5f}")
+    plt.title("RMSE Test: "+str(formatted_error_test))
 
     plt.subplot(325)
-    plt.margins(0.05)
-    plt.plot(abs(y_train - yhat_train))
-    plt.legend(["Residuals"])
-    plt.title("Train")
+    plt.margins(0.02)
+    plt.plot(abs(y_train - yhat_train), linestyle='--', linewidth=1, marker='o', markersize=4)
+    plt.legend(["Train Residuals"])
 
     plt.subplot(326)
-    plt.margins(0.05)
-    plt.plot(abs(y_test - yhat_test))
-    plt.legend(["Residuals"])
-    plt.title("Test")
+    plt.margins(0.02)
+    plt.plot(abs(y_test - yhat_test), linestyle='--', linewidth=1, marker='o', markersize=4)
+    plt.legend(["Test Residuals"])
 
     plt.subplots_adjust(left=0.1,
                     bottom=0.1,
@@ -55,4 +58,4 @@ def save_performance_graph(
                     top=0.9,
                     wspace=0.4,
                     hspace=0.4)
-    # plt.savefig(path+str(id)+".png", transparent=True)
+    plt.savefig(path+str(id)+".png", transparent=True)
