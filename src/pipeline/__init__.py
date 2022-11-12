98aefdd94e060fbe4ad9321f74c618a7e9216d33
lcorallo <l.corallo2@gmail.com>
2022-11-12 17:27:47 +0100

    Test Pipeline: Linear and SVR

--- src/pipeline/__init__.py
+++ src/pipeline/__init__.py
@@ -14,7 +14,7 @@ class IPipeline():
     def __execute__(self, series, goal: ForecastingGoal):
         raise ValueError("Method not implemented")
     
-    def __test_execute__(self, goal: ForecastingGoal):
+    def __test_execute__(self):
         raise ValueError("Method not implemented")
 
 
